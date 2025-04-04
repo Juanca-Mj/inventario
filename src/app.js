057116b03509c02cbd743d19a6b8aa1eba9c9c00
Juanca-Mj <Jcmejiasanchez05@gmail.com>
2025-04-04 14:27:30 -0500

    Actualización de app.js y config.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
-
+import categoriaRoute from "./routes/categorias.routes.js"
 const app = express();
 
 app.set("port",5000)
+
+app.use("/api/categorias", categoriaRoute);
+
 export default app;
